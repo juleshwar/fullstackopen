a0ddb9ee77a1b11806df3d57decda6b6936bbc10
juleshwar.babu <juleshwar.babu@zohocorp.com>
2020-04-18 23:55:20 +0530

    Comitting the actual soultion for ex 1.1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => {
+    return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+    const template = [];
+    props.contentArray.forEach((contentFragment) => {
+        template.push(
+            <p>
+                {contentFragment.part} {contentFragment.exercises}
+            </p>
+        );
+    });
+    return template;
+};
+const Total = (props) => {
+    return <p>Number of exercises {props.totalExercises}</p>;
+};
+
+const App = () => {
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content
+                contentArray={[
+                    { part: part1, exercise: exercises1 },
+                    { part: part2, exercise: exercises2 },
+                    { part: part3, exercise: exercises3 },
+                ]}
+            />
+            <Total totalExercises={exercises1 + exercises2 + exercises3} />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
