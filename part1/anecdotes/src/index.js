843a086b49ed2484b453f1b9639a17d3cbb5d0bd
juleshwar <bjuleshwar@gmail.com>
2020-05-02 15:13:22 +0530

    Solution to ex 1.13*

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 
 const App = (props) => {
     const [selected, setSelected] = useState(0);
-
+    const [anecdotes, setAnecdotes] = useState(props.anecdotes);
+    const selectedAnecdote = anecdotes[selected];
     return (
         <>
-            <div>{props.anecdotes[selected]}</div>
+            <div>{selectedAnecdote.text}</div>
+            <div>has {selectedAnecdote.votes} vote(s)</div>
+            <button
+                onClick={() => {
+                    ++selectedAnecdote.votes;
+                    setAnecdotes(Object.assign([], anecdotes));
+                }}
+            >
+                Vote
+            </button>
             <NextButton onNext={setSelected} totalCount={anecdotes.length} text="Next Anecdote" />
         </>
     );
@@ -20,7 +30,7 @@ function randomNumberGenerator(maxCount) {
     return Math.floor(Math.random() * maxCount);
 }
 
-const anecdotes = [
+const ANECDOTES_TEXT = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -28,5 +38,9 @@ const anecdotes = [
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ];
+
+const anecdotes = ANECDOTES_TEXT.map((anecdoteText) => {
+    return { text: anecdoteText, votes: 0 };
+});
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
