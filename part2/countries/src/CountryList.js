21ece4489faa3485043c41ee82e1b1bfc1a987d8
juleshwar <bjuleshwar@gmail.com>
2020-05-31 21:20:58 +0530

    Solution for ex 2.13* Added a button to show the country

--- part2/countries/src/CountryList.js
+++ part2/countries/src/CountryList.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
-function CountryList({ countries }) {
+function CountryList({ countries, handleShowButtonClick }) {
     return (
         <>
             {
                 countries.length > 10 ?
                     <div>Too many matches. Specify another filter</div> :
-                    countries.map(country => <div key={country.alpha2Code}>{country.name}</div>)
+                    countries.map(country => {
+                        return (
+                            <div key={country.alpha3Code}>
+                                {country.name}
+                                <button onClick={() => { handleShowButtonClick(country) }}>Show</button>
+                            </div>
+                        )
+                    })
             }
         </>
     )
