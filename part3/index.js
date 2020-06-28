2558f93e0a59cccd54bfe783c947f680ced3ffc2
juleshwar <bjuleshwar@gmail.com>
2020-06-28 17:47:49 +0530

    Solution for 3.3

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { PHONEBOOK } = require('./resources');
+const { getContact } = require('./services/Utility');
+const HTTP_STATUS = require('./constants/HTTP_STATUS');
 
 const PORT = 3001;
 const PREFIX = `/api`;
@@ -16,7 +18,15 @@ server.get(`/info`, (req, res) => {
 server.get(`${PREFIX}/persons`, (req, res) => {
     res.json(PHONEBOOK);
 })
-    res.json(NOTES);
+
+server.get(`${PREFIX}/persons/:id`, (req, res) => {
+    const id = Number(req.params.id);
+    const contact = getContact(id);
+    if (contact) {
+        res.json(contact);
+    } else {
+        res.status(HTTP_STATUS.NOT_FOUND).end();
+    }
 })
 
 server.listen(PORT, () => {
