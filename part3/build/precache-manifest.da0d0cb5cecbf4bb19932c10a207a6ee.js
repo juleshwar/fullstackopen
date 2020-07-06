033987f266b26395d12767d8606c52aa3c4ce0b8
juleshwar <bjuleshwar@gmail.com>
2020-07-06 12:21:40 +0530

    Changes 1. Adding README file 2. Adding static middleware

---
+++ part3/build/precache-manifest.da0d0cb5cecbf4bb19932c10a207a6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ace15e9068aa1e0b5e67a0c77bc10f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746d045b9036eeaf1d7f",
+    "url": "/static/css/main.36077eaa.chunk.css"
+  },
+  {
+    "revision": "238734ff8eda8f347d67",
+    "url": "/static/js/2.e90385d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e90385d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746d045b9036eeaf1d7f",
+    "url": "/static/js/main.2289475a.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
