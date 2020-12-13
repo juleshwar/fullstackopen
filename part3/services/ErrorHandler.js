9f796b6f0c2b6625139cf575c25d60e019b2672a
juleshwar.babu <juleshwar.babu@zohocorp.com>
2020-12-13 19:01:04 +0530

    Solution for 3.20 Added logic to show error message in UI for PUT and POST requests

--- part3/services/ErrorHandler.js
+++ part3/services/ErrorHandler.js
@@ -5,10 +5,10 @@ const ErrorHandler = (error, request, response, next) => {
     if (error.name === 'Error') {
         /* Custom errors */
         if (new RegExp("No \\w{1,} present").test(error.message)) {
-            return response.status(HTTP_STATUS.FORBIDDEN).send({ error: error.message })
+            return response.status(HTTP_STATUS.FORBIDDEN).json({ error: error.message })
         }
     } else if (error.name === 'CastError' && error.kind == 'ObjectId') {
-        return response.status(HTTP_STATUS.NOT_FOUND).send({ error: 'Malformatted Id' })
+        return response.status(HTTP_STATUS.NOT_FOUND).json({ error: 'Malformatted Id' })
     } else if (error.name === 'ValidationError') {
         return response.status(HTTP_STATUS.FORBIDDEN).json({ error: error.message })
     }
