0b7c08677367de172f658aad7ae271cb6e1fb914
juleshwar.babu <juleshwar.babu@zohocorp.com>
2020-12-13 13:58:05 +0530

    Handling custom errors gracefully

--- part3/services/ErrorHandler.js
+++ part3/services/ErrorHandler.js
@@ -2,7 +2,12 @@ const HTTP_STATUS = require("../constants/HTTP_STATUS")
 
 const ErrorHandler = (error, request, response, next) => {
     console.error(error.message)
-    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    if (error.name === 'Error') {
+        /* Custom errors */
+        if (new RegExp("No \\w{1,} present").test(error.message)) {
+            return response.status(HTTP_STATUS.FORBIDDEN).send({ error: error.message })
+        }
+    } else if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(HTTP_STATUS.NOT_FOUND).send({ error: 'Malformatted Id' })
     } else if (error.name === 'ValidationError') {
         return response.status(HTTP_STATUS.FORBIDDEN).json({ error: error.message })
