c6708e90d9abb1b5f356aefe1e6308026261ae21
juleshwar <bjuleshwar@gmail.com>
2020-12-13 12:38:18 +0530

    Removing obsolete logic

--- part2/phonebook/src/services/UtilFunctions.js
+++ part2/phonebook/src/services/UtilFunctions.js
@@ -1,5 +1,5 @@
-function generateUniqueId() {
-    return window.crypto.getRandomValues(new Uint8Array(1))[0];
-}
+// function generateUniqueId() {
+//     return window.crypto.getRandomValues(new Uint8Array(1))[0];
+// }
 
-export { generateUniqueId }
+// export { generateUniqueId }
