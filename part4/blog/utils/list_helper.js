d1b0324fcde9902c6323b6db88aa51b08a11372e
juleshwar.babu <juleshwar.babu@zohocorp.com>
2020-12-17 00:01:31 +0530

    Solution for 4.4 Added test for checking totalLikes func

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogList) => {
+    if (!blogList.length) {
+        return 0
+    }
+
+    const likesReducer = (accumulator, next) => {
+        return accumulator + next.likes;
+    }
+    return blogList.reduce(likesReducer, 0);
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
