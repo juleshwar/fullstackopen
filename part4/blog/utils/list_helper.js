a742a01c91b2cfe00637e1aa5ac2eaedb772269d
juleshwar.babu <juleshwar.babu@zohocorp.com>
2020-12-17 00:15:35 +0530

    Solution for 4.5* Added a new func to get the favourite blog

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -13,7 +13,31 @@ const getTotalLikes = (blogList) => {
     return blogList.reduce(likesReducer, 0);
 }
 
+const getFavoriteBlog = (blogList) => {
+    if (!blogList.length) {
+        return null
+    }
+
+    let favoriteBlog;
+    for (let i = 0; i < blogList.length; i++) {
+        const blog = blogList[i];
+        if (!favoriteBlog) {
+            favoriteBlog = blog
+            continue;
+        }
+        if (blog.likes > favoriteBlog.likes) {
+            favoriteBlog = blog
+        }
+    }
+    return {
+        title: favoriteBlog.title,
+        author: favoriteBlog.author,
+        likes: favoriteBlog.likes,
+    }
+}
+
 module.exports = {
     dummy,
-    getTotalLikes
+    getTotalLikes,
+    getFavoriteBlog
 }
